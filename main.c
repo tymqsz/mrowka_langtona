41027af256bc03ec3ae94298191ef594915bbc3a
Tymon Piwowarski <topiwowarski@gmail.com>
2024-01-11 17:17:39 +0100

    czytanie argumentow z command linea

--- main.c
+++ main.c
@@ -9,40 +9,61 @@
 #include "data.h"
 #include "movement.h"
 
-int main(){
+
+int main(int argc, char** argv){
+	/* deklaracja zmiennych i czytanie argumentow */
 	board_t* board;
-	char* out_file = malloc(100);
 	FILE* f;	
 	int bounds_reached;
+
+	int rows = argc > 1 ? atoi(argv[1]) : 40;
+	int cols = argc > 2 ? atoi(argv[2]) : 40;
+	int iter = argc > 3 ? atoi(argv[3]): 1000;
+	char* fname_prefix = argc > 4 ? argv[4]: "test";
 	
-	char* fname_prefix = "test";
+
+	/* tworzenie nowego folderu o nazwie 
+	   podanego prefixu w istniejacym
+	   folderze "output_files" */
 	char* directory = malloc(50);
+	char* out_file = malloc(100);
 	strcpy(directory, "output_files/");
-	strcat(directory, fname_prefix);
-
-	int status = mkdir(directory, S_IRWXU);
-	if(status != 0){
-		printf("unable to mkdir\n");
-		return -1;
-	}
-
-	board = init_board(board, 80, 80);
 	
-	for(int i = 0; i < 10000; i++){
-		sprintf(out_file, "%s/%s_%d.txt", directory, fname_prefix, i);
+	/* jesli folder nie istnieje
+	   zostanie utworzony */
+	mkdir(directory, S_IRWXU);
+	
+	strcat(directory, fname_prefix);
+	mkdir(directory, S_IRWXU);
+
+	
+	/* inicjalizacja planszy oraz
+	   symulacja ruchow i zapisywanie
+	   stanow w petli */
+	board = init_board(board, rows, cols);
+	
+	for(int i = 0; i < iter; i++){
+		/* formatowanie nazwy pliku wyjsciowego */
+		sprintf(out_file, "%s/%s_%03d.txt", directory, fname_prefix, i);
 		f = fopen(out_file, "w");
+
 		print_to_file(f, board);
-		//usleep(100000);
+		
+		/* ruch i sprawdzenie jego poprawnosci */
 		bounds_reached = move(board);
 		if(bounds_reached){
 			printf("bounds reached\n");
 			fclose(f);
 			break;
 		}
+
 		fclose(f);
 	}
 
-	
+
+	/* zwolnienie pamieci */
+	free(out_file);
+	free(directory);
 	free_board(board);
 	return 0;
 }
